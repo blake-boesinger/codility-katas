c6137df48d65cd660ba5a4ec4e9842a38235e155
Blake Boesinger <blake@boesinger.org>
2012-08-15 17:37:21 +0100

    remove comments

--- src/main/scala/StackMachine.scala
+++ src/main/scala/StackMachine.scala
@@ -1,25 +1,5 @@
 import collection.mutable
 
-/*
-* -----------------------------------------------------------------------
-*
-* QATARLYST LIMITED
-*
-* -----------------------------------------------------------------------
-*
-* (C) Copyright 2012 Qatarlyst Limited. All rights reserved.
-*
-* NOTICE:  All information contained herein or attendant hereto is,
-*          and remains, the property of Qatarlyst Limited.  Many of the
-*          intellectual and technical concepts contained herein are
-*          proprietary to Qatarlyst Limited. Any dissemination of this
-*          information or reproduction of this material is strictly
-*          forbidden unless prior written permission is obtained
-*          from Qatarlyst Limited.
-*
-* -----------------------------------------------------------------------
-*/
-
 class StackMachine {
 
   val stack: mutable.Stack[Int] = new mutable.Stack[Int]()
