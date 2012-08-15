e18290333124b79a49f73c4b9ff90fb740cb6156
Blake Boesinger <blake@boesinger.org>
2012-08-15 17:43:34 +0100

    extract methods

--- src/main/scala/StackMachine.scala
+++ src/main/scala/StackMachine.scala
@@ -5,27 +5,32 @@ class StackMachine {
   val stack: mutable.Stack[Int] = new mutable.Stack[Int]()
 
 
-  def stack_machine_emulator(S: String): Int = {
+  def stack_machine_emulator(input: String): Int = {
 
-    for (c <- S) {
+    for (c <- input) {
       val i = c.toString
-      if (i != "+" && i != "*") {
+      if (digit(i)) {
         stack.push(i.toInt)
       } else {
-        if (performOps(i)) {
+        if (errorWhenPerformingOperation(i)) {
           return -1
         }
       }
-
     }
-
      return stack.pop()
-
   }
 
 
+  def errorWhenPerformingOperation(i: String): Boolean = {
+    performOps(i)
+  }
+
+  def digit(i: String): Boolean = {
+    i != "+" && i != "*"
+  }
+
   def performOps(i: String) : Boolean = {
-    if ( stack.size < 2) {
+    if ( notEnoughElementsInStack) {
       return true
     }
 
@@ -43,5 +48,9 @@ class StackMachine {
       stack.push(result)
     }
       return false
+  }
+
+  def notEnoughElementsInStack: Boolean = {
+    stack.size < 2
   }
 }
